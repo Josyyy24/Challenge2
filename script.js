518fc486c2e8d79864a036ab7529a7dcfd61b1fb
Josyyy24 <153039949+Josyyy24@users.noreply.github.com>
2024-01-26 02:54:10 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "I am currently going to school to pursue my associate's degree in Engineering Design. I am currently working part time as a Closing Expert for Target. I was also a CNC Machinist for about 4 years and that's what got me interested in going back to school to become an engineer and I would also like to become a programmer.";
